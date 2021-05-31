ee8edb7606ec02629075ee3726a13605c4b66a58
Varun's Notebook <varunherlekar22@gmail.com>
2021-05-31 14:52:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const App = () => {
+    return (
+		<ChatEngine
+			height='100vh'
+			userName='varun'
+			userSecret='varun'
+			projectID='f8728450-9fb6-4041-a3ef-55329560e884'
+		/>
+	);
+    }
+export default App
