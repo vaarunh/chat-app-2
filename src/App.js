30cd7d2497bcd257ed78083f190ebfba9e4c7e80
Varun's Notebook <varunherlekar22@gmail.com>
2021-06-02 19:51:33 +0530

    Project Completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
+
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = 'f8728450-9fb6-4041-a3ef-55329560e884';
+
 const App = () => {
-    return (
-		<ChatEngine
-			height='100vh'
-			projectID='f8728450-9fb6-4041-a3ef-55329560e884'
-			userName='varun'
-			userSecret='varun'
-			renderChatFeed={(chatAppProps)=> <ChatFeed{...chatAppProps} />}
-		/>
-	);
-    }
-export default App
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
